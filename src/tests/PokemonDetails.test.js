3922e6f2a2be21af3e0078b936017ec71bf10389
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-08-18 19:49:50 -0300

    adicionando um pedaço do pokemondetails

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('Teste favorite pokemons ', () => {
+  it('Verifica se ao clicar no favorito ele favorita', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByText('More details');
+    userEvent.click(moreDetails);
+    const nameDetails = screen.getByText('Pikachu Details');
+    expect(nameDetails).toBeInTheDocument();
+    expect(moreDetails).not.toBeInTheDocument();
+  });
+});
